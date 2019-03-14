779749f61b914142c7317f8b1821bb45462addf5
allaev <34710484+allaev@users.noreply.github.com>
2019-03-14 14:48:46 +0500

    install react-router, create routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
