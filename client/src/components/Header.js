6628f748d958e5f8255a169cfa11fe7306eb13ab
allaev <34710484+allaev@users.noreply.github.com>
2019-03-14 22:32:27 +0500

    install Google API, init GoogleAuth component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import GoogleAuth from './GoogleAuth';
+
 const Header = () => {
   return (
     <div>
@@ -12,6 +14,7 @@ const Header = () => {
           <Link to="/" className="item">
             All Streams
           </Link>
+          <GoogleAuth />
         </div>
       </div>
     </div>
