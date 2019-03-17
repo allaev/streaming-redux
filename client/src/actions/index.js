f743ee4fc2bed203a02fbdeb4967062d0950380f
allaev <34710484+allaev@users.noreply.github.com>
2019-03-17 15:42:22 +0500

    add fetchStreams, fetchStream, editStream and deleteStream action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -20,3 +20,35 @@ export const createStream = (formValues) => {
     dispatch({ type: 'CREATE_STREAM', payload: response.data });
   };
 };
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get('/streams');
+
+    dispatch({ type: 'FETCH_STREAMS', payload: response.data });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: 'FETCH_STREAM', payload: response.data });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({ type: 'EDIT_STREAM', payload: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: 'DELETE_STREAM', payload: id });
+  };
+};
