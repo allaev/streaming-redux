713521bfdff6c6f2a321a5275eabecc9dbc3cd07
allaev <34710484+allaev@users.noreply.github.com>
2019-03-19 15:07:12 +0500

    implement wildcard navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
           <Route path="/streams/delete" component={StreamDelete} />
-          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
           <Route path="/streams/show" component={StreamShow} />
         </div>
       </Router>
