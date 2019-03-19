a0bf00cdcd88886358e60d799458d99e3ff740f3
allaev <34710484+allaev@users.noreply.github.com>
2019-03-19 10:18:18 +0500

    implement programmatic navigation after successful form submission in createStream action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../api/streams';
+import history from '../history';
 
 export const signIn = (userId) => {
   return {
@@ -19,6 +20,7 @@ export const createStream = (formValues) => {
     const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({ type: 'CREATE_STREAM', payload: response.data });
+    history.push('/');
   };
 };
 
